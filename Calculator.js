d566a33807ad31d027cf88fc10a513d9e508220a
Jeremy Sullivan <jsullivansec95@gmail.com>
2019-01-16 08:59:36 +0000

    added functionality for multiple operators

--- Calculator.js
+++ Calculator.js
@@ -1,66 +1,65 @@
 function buttonPress(clicked_name){
   console.log(clicked_name);
   let screen = document.getElementById("screen");
-  if(screen.value.contains())
   screen.value = screen.value + document.getElementById(clicked_name).value;
 }
 
-function calculateSum(){
-  let sum = document.getElementById('screen').value
+function calculateSum2(){
+  let sum = document.getElementById('screen').value;
   let values = [];
   let funcs = [];
-  for(let i = 0; i < sum.length; i++){
+  let breakpoint=0;
+  for(let i=0; i < sum.length; i++){
     if(sum.charAt(i) == "+" || sum.charAt(i) == "-" || sum.charAt(i) == "/" || sum.charAt(i) == "*" ){
-      console.log(sum.charAt(i));
-      num1 = sum.substring(0,i);
-      for (let j=i;j<sum.length;j++){
-          if(sum.charAt(j) == "+" || sum.charAt(j) == "-" || sum.charAt(j) == "/" || sum.charAt(j) == "*" ){
-            num2 = sum.substring(2+i,j+1);
-      }
-    }
-
-      console.log(num1);
-      console.log(num2);
-      //sum.split(i)
-      switch (sum.charAt(i)) {
-        case "+":
-        add(num1,num2);
-        break;
-        case "-":
-        subtract(num1,num2);
-        break;
-        case "*":
-        mul(num1,num2);
-            break;
-        case "/":
-        divi(num1,num2);
-
-          break;
-        default:
-
-      }
-      console.log(sum.charAt(i));
+      values.push(sum.substring(breakpoint, i));
+      funcs.push(sum.charAt(i));
+      breakpoint = i + 1;
     }
   }
+  values.push(sum.substring(breakpoint,sum.length));
 
-  function add(num1,num2){
-    document.getElementById('screen').value = +num1 + +num2;
-  return +num1 + +num2;
+  while(values.length > 0 && funcs.length > 0){
+    console.log(values);
+    console.log(funcs);
+    let a = values.pop();
+    let b = values.pop();
+    let x = funcs.pop();
+
+    switch(x){
+      case "+":
+        values.push(add(a,b));
+        break;
+      case "-":
+        values.push(subtract(b,a));
+        break;
+      case "*":
+        values.push(mul(a,b));
+        break;
+      case "/":
+        values.push(divi(b,a));
+        break;
+    }
   }
-
-  function subtract(num1,num2){
-    document.getElementById('screen').value = +num1 - +num2;
-  return +num1 - +num2;
-  }
-
-  function mul(num1,num2){
-    document.getElementById('screen').value = +num1 * +num2;
-  return +num1 * +num2;
-  }
-
-
+  document.getElementById("screen").value = values.pop();
 }
- function divi(num1,num2){
-   document.getElementById('screen').value = +num1 / +num2;
- return +num1 / +num2;
- }
+
+
+function add(num1,num2){
+  //document.getElementById('screen').value = +num1 + +num2;
+return +num1 + +num2;
+}
+
+function subtract(num1,num2){
+  //document.getElementById('screen').value = +num1 - +num2;
+return +num1 - +num2;
+}
+
+function mul(num1,num2){
+  //document.getElementById('screen').value = +num1 * +num2;
+return +num1 * +num2;
+}
+
+function divi(num1,num2){
+  //document.getElementById('screen').value = +num1 / +num2;
+return +num1 / +num2;
+}
